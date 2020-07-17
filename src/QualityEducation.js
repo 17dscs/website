443052bdb3445d6756f05ceb3b77885f7d1ca993
taeeun kim <kimtaeeeny@gmail.com>
2020-07-17 12:33:36 +0900

    add component description

--- src/QualityEducation.js
+++ src/QualityEducation.js
@@ -7,8 +7,23 @@ export const QualityEducation = () => {
   }, []);
   return (
     <>
-      <section style={{ display: "flex", justifyContent: "center", alignItems: "center" }} className="quality-education">
-        <div id="dscomponent" style={{ width: "400px", height: "300px" }}></div>
+      <section className="quality-education">
+        <h1>4. QualityEducation</h1>
+        <p>
+          Put your mouse inside the canvas. Then the chart will change. Up to the chart's peak is static data about the percentage of
+          affected learners by COVID-19 and subsequent data is dynamic.
+        </p>
+        <div>
+          <div className="color-description">
+            <div className="color-square-red"></div>
+            <div>affected Learner</div>
+          </div>
+          <div className="color-description">
+            <div className="color-square-blue"></div>
+            <div>Total Learner - affected Learner</div>
+          </div>
+        </div>
+        <div id="dscomponent" style={{ width: "400px", height: "300px", border: "3px solid black" }}></div>
       </section>
     </>
   );
